fc14c365140f563ab87556744599e822edded8ba
Ashley Man <aman@z545070.local>
2020-02-13 11:53:25 -0800

    Updates

---
+++ precache-manifest.e0d0efddd7f7cde4590a3375b5431e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ff8f22931d22af5b746c3772ead79d",
+    "url": "/React_Monster_Rolodex/index.html"
+  },
+  {
+    "revision": "ad831146a5fcf3827036",
+    "url": "/React_Monster_Rolodex/static/css/main.056be0a3.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/React_Monster_Rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_Monster_Rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad831146a5fcf3827036",
+    "url": "/React_Monster_Rolodex/static/js/main.268f1c2a.chunk.js"
+  },
+  {
+    "revision": "1be1c8caee0aacc36270",
+    "url": "/React_Monster_Rolodex/static/js/runtime-main.27ca9fd1.js"
+  }
+]);
